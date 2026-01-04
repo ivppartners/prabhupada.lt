6b4e070f65cb017e79e1c538feadd0c1c739b0e1
Saulius Domarkas <programavimas@ivp.lt>
2026-01-04 19:30:39 +0200

    pataisytas serverio cors įrašas

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 4001;
 const app = express();
 
 var corsOptions = {
-  origin: ['http://localhost:3300', 'http://localhost:5173', 'http://192.168.4.156:3300', 'http://prabhupada.lt', 'https://prabhupada.lt', 'https://prabhupada.admin'],
+  origin: ['http://localhost:3300', 'http://localhost:5173', 'http://192.168.4.156:3300', 'http://prabhupada.lt', 'https://prabhupada.lt', 'http://prabhupada-admin'],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
